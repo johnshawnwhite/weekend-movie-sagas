424963c4088fdd1d97ec9ebd6221aaedd26e9a2e
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-07-25 18:26:26 -0400

    details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,29 +1,34 @@
+import { ListItemAvatar } from '@material-ui/core';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory} from "react-router-dom";
 
 
 function Details() {
 
-    const dispatch = useDispatch();
-    const details = useSelector(store => store.details);
+    const history = useHistory();
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
+    const movieID = useSelector(store => store.movieID);
+    const movies  = useSelector(store => store.movies);
+    const genres = useSelector(store => store.genres);
 
+
+    consst handleClick = () => {
+        history.push('/');
+    }
     return (
         <main>
             <h1>Details Page</h1>
-            <section className="details">
-                {details.map(details => {
-                    return (
-                        <div key={details.id} >
-                            <h3>{details.title}</h3>
-                            <img src={details.poster} alt={details.title}/>
-                        </div>
-                    );
-                })}
-            </section>
+            <h2>{movies[movieID-1].title}</h2>
+            <img src={movies[movieID-1].poster}/>
+            <h2>Genres:</h2>
+                <ul>
+                    {genres.map((item) => {
+                        return <li key={item.id}>{item.name}</li>
+                    })}
+                </ul>
+                <p>(movies[movieId-1].description)</p>
+                <button onClick={() => handleClick()}>Return</button>
         </main>
 
     );
